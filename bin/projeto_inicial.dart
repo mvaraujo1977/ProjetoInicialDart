045e219ac40b92b98da92490849cf81c16416abc
Marcelo Viana <marcelovianadearaujo@gmail.com>
2024-08-18 18:39:17 -0300

    falando sobre strings

--- bin/projeto_inicial.dart
+++ bin/projeto_inicial.dart
@@ -2,11 +2,14 @@
 
 void main(List<String> arguments) {
   int idade = 46;
-  double altura = 46;
-  bool geek = (idade == altura);
+  double peso = 81.5;
+  bool dart = true;
+  String nome = 'Marcelo Viana';
+  String esporte = ' BJJ';
 
 
 
-  print(geek);
+  print(nome + esporte);
+  print('Meu nome é $nome tenho $idade anos, peso $peso e pratico$esporte . ' );
 
 }
