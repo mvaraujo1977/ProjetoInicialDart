27735554692a34ab7f7962a6974fcf37700d30ca
Marcelo Viana <marcelovianadearaujo@gmail.com>
2024-08-21 22:17:19 -0300

    adaptando a string

--- bin/projeto_inicial.dart
+++ bin/projeto_inicial.dart
@@ -6,11 +6,16 @@ void main(List<String> arguments) {
   bool dart = true;
   String nome = 'Marcelo Viana';
   String esporte = ' BJJ';
-  String frase = 'Meu nome é $nome tenho $idade anos, peso $peso e pratico$esporte . ';
+  String frase = 'Meu nome é $nome \n tenho $idade anos, \n peso $peso e \n pratico$esporte . ';
 
 
+  List<String> listanomes = ['Ricarth', 'Natália', 'Alex', 'Andriu', 'Andre'];
+
+  int quantidade = listanomes.length;
 
   print(nome + esporte);
   print(frase );
+
+  print(listanomes[0]);
 
 }
