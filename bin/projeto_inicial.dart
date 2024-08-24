a229249c1cb00290ce0bad11e09ee6e1eb72aa5c
Marcelo Viana <marcelovianadearaujo@gmail.com>
2024-08-24 18:15:02 -0300

    adaptando a string

--- bin/projeto_inicial.dart
+++ bin/projeto_inicial.dart
@@ -8,14 +8,20 @@ void main(List<String> arguments) {
   String esporte = ' BJJ';
   String frase = 'Meu nome é $nome \n tenho $idade anos, \n peso $peso e \n pratico$esporte . ';
 
-
-  List<String> listanomes = ['Ricarth', 'Natália', 'Alex', 'Andriu', 'Andre'];
-
-  int quantidade = listanomes.length;
-
   print(nome + esporte);
   print(frase );
 
+/*------------------------------------Listas---------------------------------------------------*/
+
+  List<String> listanomes = ['Ricarth', 'Natália', 'Alex', 'Andriu', 'Andre'];
+
+  List<dynamic> marcelo = ['Marcelo Viana de Araujo', 46, 80.0, 'BJJ'];
+
+  int quantidade = listanomes.length;
+
+  String fardeListadinamica = 'Meu nome é ${marcelo[0]} tenho ${marcelo[1]} anos, peso ${marcelo[2]} e pratico ${marcelo[3]} . ';
+
   print(listanomes[0]);
+  print(fardeListadinamica);
 
 }
